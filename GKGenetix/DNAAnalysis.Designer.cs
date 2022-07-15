7b0de0fa4f619847990598c9ff67882d3a50106a
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2022-07-15 15:40:37 +0300

    Dialogs differentiated

---
+++ GKGenetix/DNAAnalysis.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace GKGenetix
+{
+    partial class DNAAnalysis
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnLoadFile;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtOutput;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnLoadFile = new System.Windows.Forms.ToolStripButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnLoadFile});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1955, 39);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(73, 36);
+            this.btnLoadFile.Text = "Load File";
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 49);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtOutput);
+            this.splitContainer1.Size = new System.Drawing.Size(1955, 863);
+            this.splitContainer1.SplitterDistance = 651;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOutput.Location = new System.Drawing.Point(0, 0);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(1300, 863);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // DNAAnalysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1564, 729);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "DNAAnalysis";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DNA Inheritance Test";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ GKGenetix/DNAAnalysis.cs
@@ -0,0 +1,61 @@
+﻿/*
+ *  "GEDKeeper", the personal genealogical database editor.
+ *  Copyright (C) 2009-2022 by Sergey V. Zhdanovskih.
+ *
+ *  This file is part of "GEDKeeper".
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.IO;
+using System.Windows.Forms;
+using GKGenetix.Core;
+
+namespace GKGenetix
+{
+    public partial class DNAAnalysis : Form, IDisplay
+    {
+        private string fFileName;
+        private DNAData fDNA;
+
+        public DNAAnalysis()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new OpenFileDialog()) {
+                dlg.Multiselect = false;
+                if (dlg.ShowDialog() == DialogResult.OK) {
+                    var file = dlg.FileNames[0];
+
+                    fFileName = file;
+                    fDNA = FileFormats.ReadAncestryDNAFile(fFileName);
+                    fDNA.DetermineSex();
+
+                    WriteLine("File name: " + Path.GetFileName(fFileName));
+                    WriteLine("Sex: " + fDNA.Sex.ToString());
+                }
+            }
+        }
+
+        public void WriteLine(string value)
+        {
+            txtOutput.Text += value;
+            txtOutput.Text += "\r\n";
+        }
+    }
+}
