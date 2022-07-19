66a6f7ed0f29ea528f1384af729c1f595a91ecf0
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2022-07-19 17:08:08 +0300

    Created GKGenetix plugin for GEDKeeper

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ Features:
 - Sex determination from raw data;
 - Determining the list of haplogroups from raw data (outdated SNP reference data);
 - Comparison of two or more files to determine relationships (ancestor/descendant) and correlation of relationships;
+- Plugin for GEDKeeper;
 
 Planned:
 - Simple (primitive?) loading of VCF files;
 - Downloading up-to-date SNP reference databases;
 - Simple charts;
-- Plugin for GEDKeeper;
--- clean.cmd
+++ clean.cmd
@@ -1,10 +1,10 @@
-rmdir .\.vs /s /q
+rmdir .\.vs /s /q 2>NUL
 
-rmdir .\GKGenetix.Core\bin /s /q
-rmdir .\GKGenetix.Core\obj /s /q
+rmdir .\GKGenetix.Core\bin /s /q 2>NUL
+rmdir .\GKGenetix.Core\obj /s /q 2>NUL
 
-rmdir .\GKGenetix.UI.WinForms\bin /s /q
-rmdir .\GKGenetix.UI.WinForms\obj /s /q
+rmdir .\GKGenetix.UI.WinForms\bin /s /q 2>NUL
+rmdir .\GKGenetix.UI.WinForms\obj /s /q 2>NUL
 
-rmdir .\GKGenetix\bin /s /q
-rmdir .\GKGenetix\obj /s /q
+rmdir .\GKGenetix\bin /s /q 2>NUL
+rmdir .\GKGenetix\obj /s /q 2>NUL
