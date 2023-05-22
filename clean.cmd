629a6c4077a6c9de8518d7a229059e9516302295
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2023-05-22 23:08:02 +0300

    Update for cleaning

--- clean.cmd
+++ clean.cmd
@@ -6,6 +6,9 @@ rmdir .\GKGenetix.Core\obj /s /q 2>NUL
 rmdir .\GKGenetix.UI.WinForms\bin /s /q 2>NUL
 rmdir .\GKGenetix.UI.WinForms\obj /s /q 2>NUL
 
+rmdir .\GKGenetix.UI.EtoForms\bin /s /q 2>NUL
+rmdir .\GKGenetix.UI.EtoForms\obj /s /q 2>NUL
+
 rmdir .\GKGenetix\bin /s /q 2>NUL
 rmdir .\GKGenetix\obj /s /q 2>NUL
 
