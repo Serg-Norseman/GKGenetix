3f680a740d2d95958585313b8789dcda92accf10
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2025-05-21 19:00:29 +0300

    Added original authorship header to all files

--- Forms/GKMainFrm.cs
+++ Forms/GKMainFrm.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Genetic Genealogy Kit (GGK), v1.2
+ * Copyright © 2014 by Felix Chandrakumar
+ * License: MIT License (http://opensource.org/licenses/MIT)
+ */
+
+using System;
 using System.ComponentModel;
 using System.Data;
 using System.IO;
