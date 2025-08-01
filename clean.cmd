efc0c0b76343a932d4f917b520b4ec67abcedee8
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2025-08-01 23:38:47 +0300

    Reorganization

--- clean.cmd
+++ clean.cmd
@@ -12,8 +12,5 @@ rmdir .\GKGenetix.UI.EtoForms\obj /s /q 2>NUL
 rmdir .\GKGenetix\bin /s /q 2>NUL
 rmdir .\GKGenetix\obj /s /q 2>NUL
 
-rmdir .\GKGenetixPlugin\bin /s /q 2>NUL
-rmdir .\GKGenetixPlugin\obj /s /q 2>NUL
-
 rmdir .\GKGenetix.Tests\bin /s /q 2>NUL
 rmdir .\GKGenetix.Tests\obj /s /q 2>NUL
